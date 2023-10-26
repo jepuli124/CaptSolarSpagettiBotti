441cabe78b9ed1ed75803fcf3b94e5a8bd4606cf
Eetu Asikainen <eetu.asikainen@visma.com>
2023-10-26 07:38:17 +0300

    Base models

---
+++ src/apiwrapper/models.py
@@ -0,0 +1,92 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
+
+
+@dataclass
+class Coordinates:
+    x: int
+    y: int
+
+
+class CompassDirection(Enum):
+    North = 0,
+    NorthEast = 1,
+    East = 2,
+    SouthEast = 3,
+    South = 4,
+    SouthWest = 5,
+    West = 6,
+    NorthWest = 7
+
+
+class CellType(Enum):
+    Empty = "O",
+    OutOfVision = "X",
+    AudioSignature = "?",
+    HitBox = "H",
+    Ship = "S",
+    Projectile = "P"
+
+
+@dataclass
+class HitBoxData:
+    entity_id: str
+
+
+@dataclass
+class EntityData:
+    id: str
+    position: Coordinates
+    direction: CompassDirection
+
+
+@dataclass
+class ShipData(EntityData):
+    health: Optional[int]
+    heat: Optional[int]
+
+
+@dataclass
+class ProjectileData(EntityData):
+    velocity: int
+    mass: int
+
+
+@dataclass
+class Cell:
+    cell_type: CellType
+    data: None | HitBoxData | ShipData | ProjectileData
+
+
+@dataclass
+class GameState:
+    turn_number: int
+    game_map: list[list[Cell]]
+
+
+class ActionType(Enum):
+    Move = 0,
+    Turn = 1,
+    Shoot = 2
+
+
+@dataclass
+class MoveActionData:
+    distance: int
+
+
+@dataclass
+class TurnActionData:
+    direction: CompassDirection
+
+
+@dataclass
+class ShootActionData:
+    mass: int
+    speed: int
+
+
+@dataclass
+class Command:
+    pass
--- src/team_ai.py
+++ src/team_ai.py
@@ -1,8 +1,12 @@
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from src.apiwrapper.events import GameState, Command
+    from src.apiwrapper.models import GameState, Command
 
 
-def process_tick(game_state: GameState) -> Command:
+def process_tick(context, game_state: GameState) -> Command:
+    pass
+
+
+if __name__ == '__main__':
     pass
